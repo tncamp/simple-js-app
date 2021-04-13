ad8673e7623bd2476e20249a5d73ec044ca846ca
tncamp <80801111+tncamp@users.noreply.github.com>
2021-04-12 22:50:27 -0500

    Added to js
    Created a new variable pokemonList. Added objects to the array as Pokemon. Created four keys for each object and added their specific values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-alert('Hello world');
-let favFood = 'Pasta';
-document.write(favFood);
+let pokemonList = [];
+  pokemonList = [
+    {
+      name: 'Pikachu',
+      height: '1\'04"',
+      type: 'electric',
+      abilities:'static'
+    },
+    {
+      name: 'Ivysaur',
+      height: '3\'03"',
+      type: ['grass' , 'poison'],
+      abilities: 'overgrow'
+    },
+    {
+      name: 'Squirtle',
+      height:'1\08"',
+      type: 'water',
+      abilities: 'torrent'
+    }
+];
