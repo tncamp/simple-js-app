4ada38e458d31705aec09c751156b9ec3778148f
tncamp <80801111+tncamp@users.noreply.github.com>
2021-04-14 19:54:17 -0500

    Added to js
    Created for loop and conditional to check if height is above certain value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
+//creates array of pokemon and their characteristics
 let pokemonList = [];
   pokemonList = [
     {
       name: 'Pikachu',
-      height: '1\'04"',
+      height: '1.4',
       type: 'electric',
       abilities:'static'
     },
     {
       name: 'Ivysaur',
-      height: '3\'03"',
+      height: '3.3',
       type: ['grass' , 'poison'],
       abilities: 'overgrow'
     },
     {
       name: 'Squirtle',
-      height:'1\08"',
+      height:'1.8',
       type: 'water',
       abilities: 'torrent'
-    }
+    },
 ];
+
+for (let i = 0; (pokemonList.length) ; i++){
+  if (pokemonList[i].height > 2){
+    document.write(pokemonList[i].name + ' (height:) ' + pokemonList[i].height + ' -Wow, that\'s big! ' + pokemonList[i].type + ' ' + pokemonList[i].abilities + ' ' + '<br/>')
+  }
+  else { document.write(pokemonList[i].name + ' (height:)' + pokemonList[i].height + ' ' + pokemonList[i].type + ' ' + pokemonList[i].abilities + ' ' + '<br/>')
+  }
+}
